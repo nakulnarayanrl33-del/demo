ee4e297af7a9ab34776e62eaaaccc96ebae6993c
nakulnarayanrl33-del <nakulnarayanrl33@gmail.com>
2026-05-13 01:48:19 +0530

    Add files via upload

---
+++ day1cw.js
@@ -0,0 +1,10 @@
+const customerName:" Nakul ";
+const customerReview:" The cafe is very modern";
+const review={
+    name:customerName
+    review:customerReview
+};
+function displayDetails(reviewObj){
+    const upperName=reviewObj.name.toUpperCase();
+    const 
+}
