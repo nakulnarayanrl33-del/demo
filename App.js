f651e334a95fcf9d87a13e0b754f568245e85cbc
nakulnarayanrl33-del <nakulnarayanrl33@gmail.com>
2026-06-24 12:34:50 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,32 +1,23 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const userName = "Nakul";
-
-  console.log("React app started");
-
-  const headingStyle = {
-    color: "blue",
-    textAlign: "center",
-    fontWeight: "bold",
-  };
-
   return (
-    <div className="container d-flex justify-content-center align-items-center vh-100">
-      <div className="card shadow p-4 text-center" style={{ width: "30rem" }}>
-        
-        <h1 style={headingStyle}>
-          Welcome to React Learning, {userName}
-        </h1>
-
-
-        <p className="text-primary fw-bold">
-          This is your first card with images and styles!
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-
-      </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
